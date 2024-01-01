7b313a768484e35e7bd4fc0de0d82090b863396b
Moshiur Rahman Naeem <68812831+miraz173@users.noreply.github.com>
2024-01-01 20:55:35 +0600

    Add files via upload

---
+++ mount.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# sudo mkdir /media/naeem/Personal
+# sudo mkdir /media/naeem/study
+# sudo mkdir /media/naeem/Apps
+# sudo mkdir /media/naeem/Etc
+
+echo hello naeem!
+
+sudo mount /dev/sda2 /media/naeem/Personal
+sudo mount /dev/sda4 /media/naeem/study
+sudo mount /dev/sda5 /media/naeem/Apps
+sudo mount /dev/sda3 /media/naeem/Etc
+
+echo done doing it.
+#read a
+echo have a rough day, boy!
